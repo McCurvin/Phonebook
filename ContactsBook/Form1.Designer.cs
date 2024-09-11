be7f464759a55ec9d7ac5354eaed2fab37d4d076
Mc Curvin Royeras <roynivruc@gmail.com>
2024-09-11 13:07:13 +0800

    Add project files.

---
+++ ContactsBook/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace ContactsBook
+{
+    partial class Phonebook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.contactIdTextBox = new System.Windows.Forms.TextBox();
+            this.phoneNumTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.contactIdLabel = new System.Windows.Forms.Label();
+            this.phoneNumberLabel = new System.Windows.Forms.Label();
+            this.emailLabel = new System.Windows.Forms.Label();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.contactsDataGrid = new System.Windows.Forms.DataGridView();
+            this.phoneBook_Label = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.contactsDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // newButton
+            // 
+            this.newButton.BackColor = System.Drawing.Color.Salmon;
+            this.newButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.newButton.Font = new System.Drawing.Font("Perpetua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newButton.Location = new System.Drawing.Point(157, 283);
+            this.newButton.Margin = new System.Windows.Forms.Padding(4);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(147, 31);
+            this.newButton.TabIndex = 0;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = false;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(159, 134);
+            this.nameTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(305, 22);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.BackColor = System.Drawing.Color.MistyRose;
+            this.nameLabel.Font = new System.Drawing.Font("Perpetua", 9.75F);
+            this.nameLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.nameLabel.Location = new System.Drawing.Point(21, 134);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(129, 25);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "Name:";
+            this.nameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.nameLabel.Click += new System.EventHandler(this.firstNameLabel_Click);
+            // 
+            // contactIdTextBox
+            // 
+            this.contactIdTextBox.Location = new System.Drawing.Point(159, 166);
+            this.contactIdTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.contactIdTextBox.Name = "contactIdTextBox";
+            this.contactIdTextBox.Size = new System.Drawing.Size(305, 22);
+            this.contactIdTextBox.TabIndex = 3;
+            // 
+            // phoneNumTextBox
+            // 
+            this.phoneNumTextBox.Location = new System.Drawing.Point(159, 198);
+            this.phoneNumTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.phoneNumTextBox.Name = "phoneNumTextBox";
+            this.phoneNumTextBox.Size = new System.Drawing.Size(305, 22);
+            this.phoneNumTextBox.TabIndex = 4;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(159, 230);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(305, 22);
+            this.emailTextBox.TabIndex = 5;
+            this.emailTextBox.TextChanged += new System.EventHandler(this.emailTextBox_TextChanged);
+            // 
+            // contactIdLabel
+            // 
+            this.contactIdLabel.BackColor = System.Drawing.Color.MistyRose;
+            this.contactIdLabel.Font = new System.Drawing.Font("Perpetua", 9.75F);
+            this.contactIdLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.contactIdLabel.Location = new System.Drawing.Point(21, 166);
+            this.contactIdLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.contactIdLabel.Name = "contactIdLabel";
+            this.contactIdLabel.Size = new System.Drawing.Size(129, 25);
+            this.contactIdLabel.TabIndex = 6;
+            this.contactIdLabel.Text = "Contact ID:";
+            this.contactIdLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // phoneNumberLabel
+            // 
+            this.phoneNumberLabel.Font = new System.Drawing.Font("Perpetua", 9.75F);
+            this.phoneNumberLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.phoneNumberLabel.Location = new System.Drawing.Point(21, 198);
+            this.phoneNumberLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.phoneNumberLabel.Name = "phoneNumberLabel";
+            this.phoneNumberLabel.Size = new System.Drawing.Size(129, 25);
+            this.phoneNumberLabel.TabIndex = 7;
+            this.phoneNumberLabel.Text = "Phone Number:";
+            this.phoneNumberLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.phoneNumberLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // emailLabel
+            // 
+            this.emailLabel.Font = new System.Drawing.Font("Perpetua", 9.75F);
+            this.emailLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.emailLabel.Location = new System.Drawing.Point(21, 230);
+            this.emailLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.emailLabel.Name = "emailLabel";
+            this.emailLabel.Size = new System.Drawing.Size(129, 25);
+            this.emailLabel.TabIndex = 8;
+            this.emailLabel.Text = "Email:";
+            this.emailLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // loadButton
+            // 
+            this.loadButton.BackColor = System.Drawing.Color.Salmon;
+            this.loadButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.loadButton.Font = new System.Drawing.Font("Perpetua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loadButton.Location = new System.Drawing.Point(157, 315);
+            this.loadButton.Margin = new System.Windows.Forms.Padding(4);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(147, 31);
+            this.loadButton.TabIndex = 9;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = false;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.BackColor = System.Drawing.Color.Salmon;
+            this.editButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.editButton.Font = new System.Drawing.Font("Perpetua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editButton.Location = new System.Drawing.Point(319, 283);
+            this.editButton.Margin = new System.Windows.Forms.Padding(4);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(147, 31);
+            this.editButton.TabIndex = 10;
+            this.editButton.Text = "Save / Edit";
+            this.editButton.UseVisualStyleBackColor = false;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.Salmon;
+            this.deleteButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.deleteButton.Font = new System.Drawing.Font("Perpetua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deleteButton.Location = new System.Drawing.Point(319, 315);
+            this.deleteButton.Margin = new System.Windows.Forms.Padding(4);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(147, 31);
+            this.deleteButton.TabIndex = 11;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.searchTextBox.Location = new System.Drawing.Point(159, 104);
+            this.searchTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(156, 22);
+            this.searchTextBox.TabIndex = 12;
+            this.searchTextBox.Text = "Search Number";
+            this.searchTextBox.TextChanged += new System.EventHandler(this.searchTextBox_TextChanged);
+            // 
+            // contactsDataGrid
+            // 
+            this.contactsDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.contactsDataGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.contactsDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.contactsDataGrid.Location = new System.Drawing.Point(513, 74);
+            this.contactsDataGrid.Margin = new System.Windows.Forms.Padding(4);
+            this.contactsDataGrid.Name = "contactsDataGrid";
+            this.contactsDataGrid.RowHeadersWidth = 51;
+            this.contactsDataGrid.Size = new System.Drawing.Size(529, 338);
+            this.contactsDataGrid.TabIndex = 13;
+            this.contactsDataGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.contactsDataGrid_CellContentClick);
+            this.contactsDataGrid.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.contactsDataGrid_CellDoubleClick);
+            // 
+            // phoneBook_Label
+            // 
+            this.phoneBook_Label.Font = new System.Drawing.Font("Microsoft Uighur", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.phoneBook_Label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.phoneBook_Label.Location = new System.Drawing.Point(33, 30);
+            this.phoneBook_Label.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.phoneBook_Label.Name = "phoneBook_Label";
+            this.phoneBook_Label.Size = new System.Drawing.Size(173, 50);
+            this.phoneBook_Label.TabIndex = 14;
+            this.phoneBook_Label.Text = "Phonebook";
+            this.phoneBook_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // searchButton
+            // 
+            this.searchButton.BackColor = System.Drawing.Color.Salmon;
+            this.searchButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.searchButton.Font = new System.Drawing.Font("Perpetua", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(235, 354);
+            this.searchButton.Margin = new System.Windows.Forms.Padding(4);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(147, 31);
+            this.searchButton.TabIndex = 15;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = false;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Phonebook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(1087, 450);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.phoneBook_Label);
+            this.Controls.Add(this.contactsDataGrid);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.emailLabel);
+            this.Controls.Add(this.phoneNumberLabel);
+            this.Controls.Add(this.contactIdLabel);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.phoneNumTextBox);
+            this.Controls.Add(this.contactIdTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.newButton);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Phonebook";
+            this.Text = "Phonebook";
+            this.Load += new System.EventHandler(this.Phonebook_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.contactsDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox contactIdTextBox;
+        private System.Windows.Forms.TextBox phoneNumTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label contactIdLabel;
+        private System.Windows.Forms.Label phoneNumberLabel;
+        private System.Windows.Forms.Label emailLabel;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.DataGridView contactsDataGrid;
+        private System.Windows.Forms.Label phoneBook_Label;
+        private System.Windows.Forms.Button searchButton;
+    }
+}
+
---
+++ ContactsBook/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactsBook
+{
+    
+    public partial class Phonebook : Form
+    {
+        SqlConnection conner;
+        SqlCommand cmd;
+        SqlDataReader reader;
+
+
+        DataTable contacts = new DataTable();
+        bool editing = false;
+        string stringConnection = "Server=ACER;Database=curvin;Trusted_Connection=True;";
+        public Phonebook()
+        {
+            InitializeComponent();
+        }
+
+        private void firstNameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void LoadContactsFromDatabase()
+        {
+            using (SqlConnection con = new SqlConnection(stringConnection))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "SELECT name, ContactID, phoneNumber, email FROM curvinPhoneBookTable";  // Replace "Contacts" with your actual table name
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    contacts.Clear();
+
+                    while (reader.Read())
+                    {
+                        contacts.Rows.Add(reader["Name"], reader["ContactID"], reader["phoneNumber"], reader["Email"]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message);
+                }
+            }
+        }
+
+        private void Phonebook_Load(object sender, EventArgs e)
+        {
+            contacts.Columns.Add("Name");
+            contacts.Columns.Add("Contact ID");
+            contacts.Columns.Add("Phone");
+            contacts.Columns.Add("Email");
+
+            LoadContactsFromDatabase();
+
+            // Set data source
+            contactsDataGrid.DataSource = contacts;
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            nameTextBox.Text = "";
+            contactIdTextBox.Text = "";
+            phoneNumTextBox.Text = "";
+            emailTextBox.Text = "";
+
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            LoadContactsFromDatabase();
+        }
+
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(stringConnection))
+            {
+                try
+                {
+                    con.Open();
+                    string query;
+
+                    if (editing) // Update existing record
+                    {
+                        query = "UPDATE curvinPhoneBookTable SET name = @Name, phoneNumber = @Phone, email = @Email WHERE ContactID = @ContactID";
+                    }
+                    else // Insert new record
+                    {
+                        query = "INSERT INTO curvinPhoneBookTable (name, ContactID, phoneNumber, email) VALUES (@Name, @ContactID, @Phone, @Email)";
+                    }
+
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Name", nameTextBox.Text);
+                    cmd.Parameters.AddWithValue("@ContactID", contactIdTextBox.Text);
+                    cmd.Parameters.AddWithValue("@Phone", phoneNumTextBox.Text);
+                    cmd.Parameters.AddWithValue("@Email", emailTextBox.Text);
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show(editing ? "Contact updated successfully." : "New contact added successfully.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error saving data: " + ex.Message);
+                }
+            }
+
+            // Update the grid
+            if (editing)
+            {
+                contacts.Rows[contactsDataGrid.CurrentCell.RowIndex]["Name"] = nameTextBox.Text;
+                contacts.Rows[contactsDataGrid.CurrentCell.RowIndex]["Contact ID"] = contactIdTextBox.Text;
+                contacts.Rows[contactsDataGrid.CurrentCell.RowIndex]["Phone"] = phoneNumTextBox.Text;
+                contacts.Rows[contactsDataGrid.CurrentCell.RowIndex]["Email"] = emailTextBox.Text;
+            }
+            else
+            {
+                contacts.Rows.Add(nameTextBox.Text, contactIdTextBox.Text, phoneNumTextBox.Text, emailTextBox.Text);
+            }
+
+            // Clear form fields
+            nameTextBox.Text = "";
+            contactIdTextBox.Text = "";
+            phoneNumTextBox.Text = "";
+            emailTextBox.Text = "";
+            editing = false;
+        }
+
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (contactsDataGrid.CurrentCell != null)
+            {
+                string contactID = contacts.Rows[contactsDataGrid.CurrentCell.RowIndex]["Contact ID"].ToString();
+
+                using (SqlConnection con = new SqlConnection(stringConnection))
+                {
+                    try
+                    {
+                        con.Open();
+                        string query = "DELETE FROM curvinPhoneBookTable WHERE ContactID = @ContactID";
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        cmd.Parameters.AddWithValue("@ContactID", contactID);
+                        cmd.ExecuteNonQuery();
+
+                        // Remove from DataGridView
+                        contacts.Rows[contactsDataGrid.CurrentCell.RowIndex].Delete();
+
+                        MessageBox.Show("Contact deleted successfully.");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error deleting contact: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+
+
+        private void contactsDataGrid_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            nameTextBox.Text = contacts.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[0].ToString();
+            contactIdTextBox.Text = contacts.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[1].ToString();
+            phoneNumTextBox.Text = contacts.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[2].ToString();
+            emailTextBox.Text = contacts.Rows[contactsDataGrid.CurrentCell.RowIndex].ItemArray[3].ToString();
+            editing = true;
+        }
+
+        private void searchTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void emailTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contactsDataGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string search = searchTextBox.Text;
+
+            using (SqlConnection con = new SqlConnection(stringConnection))
+            {
+                try
+                {
+                    con.Open();
+                    // Use a parameterized query to prevent SQL injection
+                    string query = "SELECT * FROM curvinPhoneBookTable WHERE phoneNumber = @PhoneNumber";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", search); // Add the parameter for the search query
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // Clear the DataGridView to avoid showing old data
+                    contacts.Clear();
+
+                    if (reader.Read()) // If a record is found
+                    {
+                        // Populate the DataGridView (optional)
+                        contacts.Rows.Add(reader["Name"], reader["ContactID"], reader["phoneNumber"], reader["Email"]);
+
+                        // Load data into text boxes
+                        nameTextBox.Text = reader["Name"].ToString();
+                        contactIdTextBox.Text = reader["ContactID"].ToString();
+                        phoneNumTextBox.Text = reader["phoneNumber"].ToString();
+                        emailTextBox.Text = reader["Email"].ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No record found.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message);
+                }
+            }
+        }
+
+    }
+}
